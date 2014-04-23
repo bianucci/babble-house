4ff6ddfeda805eb4de5a84b5eaf386941ab9381f
tchuprina <tchuprina@gmail.com>
2014-04-23 22:44:29 +0200

    Removed the GPIO lines from Hello World

--- BabbleHouse/src/com/ffh/babblehouse/BabbleHouseUI.java
+++ BabbleHouse/src/com/ffh/babblehouse/BabbleHouseUI.java
@@ -8,6 +8,7 @@ import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
@@ -23,22 +24,19 @@ public class BabbleHouseUI extends UI {
 	@Override
 	protected void init(VaadinRequest request) {
 		
-		// Begin of Pin interaction
-		GpioController gpioController = GpioFactory.getInstance();
-		//Set GPIO 0 to LOW
-		final GpioPinDigitalOutput pin = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_00,"My LED", PinState.LOW);
-		// Create a button
-		Button buttonOnOff = new Button("On/Off", new Button.ClickListener () {
-			// Actions performed when button is pressed
-			public void buttonClick(Button.ClickEvent event){
-				pin.toggle();
-			}
-		});
-				
 		final VerticalLayout layout = new VerticalLayout();
 		layout.setMargin(true);
 		setContent(layout);
-
+		
+		Button buttonOnOff = new Button("On/Off", new Button.ClickListener () {
+			// Actions performed when button is pressed
+			public void buttonClick(Button.ClickEvent event){
+				
+				layout.addComponent(new Label("Test")); 
+				//pin.toggle();
+			}
+		});
+		
 		layout.addComponent(buttonOnOff);
 	}
 
