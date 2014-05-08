8045118813ae25111117f2b099027859ef4b556d
zziwabaker@gmail.com <zziwabaker@gmail.com>
2014-05-08 19:38:27 +0200

    LATEST

--- BabbleHouse/src/com/ffh/babblehouse/controller/BBNodes/Reader.java
+++ BabbleHouse/src/com/ffh/babblehouse/controller/BBNodes/Reader.java
@@ -4,32 +4,14 @@ import com.ffh.babblehouse.data.Message;
 
 public class Reader extends Thread implements IReader {
 
-	IGateway gateway;
-	public Reader(IGateway gateway){
-		this.gateway=gateway;
-	}
-	
 	@Override
 	public Message readMessage() {
-	
+		// TODO Auto-generated method stub
 		return null;
 	}
+
 	
 	
-	private Message processMsg(){
-		
-		// message type
-		
-		int b100;
-		
-	byte[] readbyte=	gateway.comListner();
-	String readybyte= readbyte.toString();
-	int Msg_type;
-	 int Zid_sender=readybyte.substring(0,3);
-	 int Zid_receive;
-	 String Payload;
-	
-	}
 	
 	
 
