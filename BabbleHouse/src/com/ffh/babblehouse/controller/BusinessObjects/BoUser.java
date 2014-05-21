bdec48ecac97c5cf417ee7112d6465651d35ec61
Bruno A. dos Santos <brunoamancio.ti@gmail.com>
2014-05-21 19:26:39 +0200

    Removed redundancy of code in BoUser.

--- BabbleHouse/src/com/ffh/babblehouse/controller/BusinessObjects/BoUser.java
+++ BabbleHouse/src/com/ffh/babblehouse/controller/BusinessObjects/BoUser.java
@@ -4,14 +4,6 @@ import com.ffh.babblehouse.controller.repositories.UserRepository;
 import com.ffh.babblehouse.model.DtoUser;
 
 public class BoUser extends BoBase<DtoUser>{
-
-	public void Register(DtoUser newUser){
-		this.repository.saveOrUpdate(newUser);
-	}
-	
-	public void Unregister(DtoUser user){
-		this.repository.delete(user);
-	}
 	
 	public boolean Validate(DtoUser userBeingVerified){
 		UserRepository userRepository = new UserRepository();
