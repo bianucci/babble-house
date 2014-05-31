15bdb3af0c8f8581ac74ca1866822d20e3643697
claudiob <DesireHtc54@gmail.com>
2014-05-31 16:13:04 +0200

    added file illustrating how to receive/send service messages

---
+++ BabbleHouse/test/com/ffh/babblehouse/messaging/SendReceiveServiceMessages.java
@@ -0,0 +1,87 @@
+package com.ffh.babblehouse.messaging;
+
+import com.ffh.babblehouse.controller.BBNodes.UARTMessageProtos.Service;
+import com.ffh.babblehouse.controller.BBNodes.UARTMessageProtos.Service.Builder;
+import com.ffh.babblehouse.controller.BBNodes.UARTMessageProtos.Service.ServiceType;
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import jssc.SerialPort;
+import jssc.SerialPortException;
+import jssc.SerialPortTimeoutException;
+
+public class SendReceiveServiceMessages {
+
+	private static class Sender extends Thread {
+		public void run() {
+			while (true) {
+
+				Builder serviceBuilder = Service.newBuilder();
+				serviceBuilder.setInfo("" + System.currentTimeMillis());
+				serviceBuilder.setServiceId(1);
+				serviceBuilder.setServiceType(ServiceType.SENSOR);
+				serviceBuilder.setServiceGroupId(2);
+
+				Service service = serviceBuilder.build();
+				byte[] message = service.toByteArray();
+				int length = message.length;
+
+				try {
+					// send one byte containing the length of the next
+					// message
+					serialPort.writeByte((byte) length);
+
+					// send the next message
+					serialPort.writeBytes(message);
+
+					System.out.println("SENT FOLLOWING PROTOBUF MESSAGE:");
+					System.out.println(service.toString());
+
+					Thread.sleep(1000);
+
+				} catch (SerialPortException e) {
+					e.printStackTrace();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+			}
+		}
+	}
+
+	private static class Receiver extends Thread {
+		public void run() {
+			while (true) {
+				byte[] isRead = new byte[100];
+				Service service = null;
+				try {
+					int size = serialPort.readBytes(1)[0];
+					if (size != 0)
+						isRead = serialPort.readBytes(size);
+					service = UARTMessageProtos.Service.parseFrom(isRead);
+				} catch (SerialPortException e) {
+					e.printStackTrace();
+				} catch (InvalidProtocolBufferException e) {
+					e.printStackTrace();
+				}
+				System.err.println("RECEIVED FOLLOWING PROTOBUF MESSAGE:");
+				System.err.println(service.toString());
+			}
+		}
+
+	}
+
+	// static SerialPort serialPort = new SerialPort("COM4");
+	static SerialPort serialPort = new SerialPort("COM5");
+
+	public static void main(String[] args) throws SerialPortException,
+			InterruptedException, SerialPortTimeoutException {
+		serialPort.openPort();
+		serialPort.setParams(38400, 8, 1, 0);
+
+		Receiver r = new Receiver();
+		Sender s = new Sender();
+
+		r.start();
+		s.start();
+	}
+}
