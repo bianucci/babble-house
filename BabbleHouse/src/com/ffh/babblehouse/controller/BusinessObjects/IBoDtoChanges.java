08a299ec6e8e7acb8c957facb9a7a121042c4de5
Bruno A. dos Santos <brunoamancio.ti@gmail.com>
2014-06-11 14:08:30 +0200

    Removed Unnecessary Interface in BO layer.

--- BabbleHouse/src/com/ffh/babblehouse/controller/BusinessObjects/IBoDtoChanges.java
+++
@@ -1,9 +0,0 @@
-package com.ffh.babblehouse.controller.BusinessObjects;
-
-import com.ffh.babblehouse.model.DtoDevice;
-import com.ffh.babblehouse.model.DtoSensor;
-
-public interface IBoDtoChanges {
-	void ChangeDeviceValues(DtoDevice dtoDevice);
-	void ChangeSensorValues(DtoSensor dtoSensor);
-}
