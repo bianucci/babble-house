f33fb644a2df0519993d9a3c5f3996c572315639
Bruno A. dos Santos <brunoamancio.ti@gmail.com>
2014-06-13 23:50:38 +0200

    Removed import of DtoValue in IBBDataBridge.

--- BabbleHouse/src/com/ffh/babblehouse/controller/BBNodes/IBBDataBridge.java
+++ BabbleHouse/src/com/ffh/babblehouse/controller/BBNodes/IBBDataBridge.java
@@ -1,7 +1,6 @@
 package com.ffh.babblehouse.controller.BBNodes;
 
 import com.ffh.babblehouse.model.DtoDevice;
-import com.ffh.babblehouse.model.DtoValue;
 
 public interface IBBDataBridge {
 	void changeDeviceStatus(DtoDevice dtoDevice);
