dea8e31eec9e231cb2a4c0c514feef322ca1ef5a
claudiob <DesireHtc54@gmail.com>
2014-06-17 23:13:25 +0200

    added basic logic of application state switching

--- firmware-wsn/src/app.c
+++ firmware-wsn/src/app.c
@@ -3,43 +3,70 @@
 #include <app.h>
 #include <usart.h>
 
-static AppState_t appState = APP_INIT_STATE;
+static AppState_t appState = APP_INIT_SYSTEM;
 
 void APL_TaskHandler(void)
 {
 	switch(appState){
 		
 		case APP_INIT_SYSTEM:
+			#if CS_DEVICE_TYPE==DEV_TYPE_COORDINATOR
+				appState=APP_INIT_UART;
+			#else
+				appState=APP_INIT_SENSORS;
+			#endif
 		break;
 		
 		case APP_INIT_UART:
+			appState=APP_START_NETWORK;
 		break;
 		
 		case APP_INIT_SENSORS:
-		break;
-		
-		case APP_INIT_ENDPOINT:
+			appState=APP_START_NETWORK;
 		break;
 		
 		case APP_START_NETWORK:
+			#if CS_DEVICE_TYPE==DEV_TYPE_COORDINATOR
+				appState=APP_IDLE;
+			#else
+				appState=APP_INIT_ENDPOINT;
+			#endif
 		break;
 		
-		case APP_ZGBE_RECVD:
-		break;
-		
-		case APP_UART_RECVD:
+		case APP_INIT_ENDPOINT:
+			appState=APP_SEND_BEACON;
 		break;
 		
 		case APP_SEND_BEACON:
+			appState=APP_IDLE;
+		break;
+		
+		case APP_UART_RCVD:
+			appState=APP_SEND_SERVICE_REQST;
 		break;
 		
 		case APP_SEND_SERVICE_REQST:
+			appState=APP_IDLE;
 		break;
 		
-		case APP_SEND_SERVICE_RSPNS:
+		case APP_ZGBE_RCVD:
+			#if CS_DEVICE_TYPE==DEV_TYPE_COORDINATOR
+				appState=APP_UART_SEND;
+			#else
+				appState=APP_READ_ADC;
+			#endif
+		break;
+		
+		case APP_UART_SEND:
+			appState=APP_IDLE;
 		break;
 		
 		case APP_READ_ADC:
+			appState=APP_SEND_SERVICE_RSPNS;
+		break;
+		
+		case APP_SEND_SERVICE_RSPNS:
+			appState=APP_IDLE;
 		break;
 		
 		case APP_IDLE:
