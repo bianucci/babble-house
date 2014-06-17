4009a710bb01c6ee4fd9844f5b0eb63a9efcdfe2
claudiob <DesireHtc54@gmail.com>
2014-06-18 00:01:12 +0200

    custom uart messaging lib is compiling succesfully

--- firmware-wsn/src/messaging.c
+++ firmware-wsn/src/messaging.c
@@ -1,20 +1,16 @@
-
+#include <messaging.h>
 
 static uint8_t Rx_Buffer[RX_BUFFER_SIZE];
 static uint8_t Tx_Buffer[TX_BUFFER_SIZE];
 static uint8_t buffer[RX_BUFFER_SIZE];
 
+Beacon my_beacon;
+HAL_UsartDescriptor_t usart;
 
-static Beacon my_beacon;
-
-static uint8_t beacon_sent=0;
-
-
-static HAL_UsartDescriptor_t usart;
-
-static int buffer_counter = 0;
-static uint8_t message_length = 0;
-
+uint8_t buffer_counter = 0;
+uint8_t message_length = 0;
+uint8_t last_msg[RX_BUFFER_SIZE]; 
+uint8_t last_msg_length=0;
 
 void usartRcvd(uint8_t size)
 {
@@ -35,66 +31,13 @@ void usartRcvd(uint8_t size)
 		if(buffer_counter==message_length){
 			memcpy(last_msg, buffer, message_length);
 			last_msg_length=message_length;
-			
-			//HAL_WriteUsart(&usart,&last_msg_length, sizeof(last_msg_length));
-			//HAL_WriteUsart(&usart,last_msg,last_msg_length);
-			
 			buffer_counter=0;
 			message_length=0;
 		}
 	}
 }
 
-static void printMessageFired(){
-
-	// DECODING LAST MESSAGE RECEIVED
-	UARTMessage uart_message;
-	/* Create a stream that reads from the buffer. */
-    pb_istream_t istream = pb_istream_from_buffer(last_msg, last_msg_length);
-	/* Now we are ready to decode the message. */
-	bool status = pb_decode(&istream, UARTMessage_fields, &uart_message);
-    
-	Service s = uart_message.service;
-	
-	// CREATING A NEW SERVICE OBJECT
-    Service service = {0};
-	service.serviceType=Service_ServiceType_SENSOR;
-	service.serviceGroupId=1;
-	service.serviceId=3;
-	service.has_value=1;
-	service.value=22;
-	service.has_info=true;
-	strcpy(service.info, "degree celcius");
-	
-	// WRAPPING SERVICE OBJECT IN A NEW UART MESSAGE
-	UARTMessage u;
-	u.has_service=true;
-	u.has_beacon=true;
-	
-	if(beacon_sent==1){
-		u.service=service;
-		u.type=UARTMessage_Type_SERVICE;
-		u.has_beacon=false;
-		beacon_sent=0;
-	} else {
-		u.beacon=my_beacon;
-		u.type=UARTMessage_Type_BEACON;
-		u.has_service=false;
-		beacon_sent=1;
-	}
-	
-	// ENCODING A MESSAGE
-	/* Create a stream that will write to our buffer. */
-	pb_ostream_t ostream = pb_ostream_from_buffer(encBuffer, sizeof(encBuffer));
-	pb_encode(&ostream, UARTMessage_fields, &u);
-	uint8_t size = ostream.bytes_written;
-	
-	// SENDING A MESSAGE
-	HAL_WriteUsart(&usart,&size,1);
-	HAL_WriteUsart(&usart,&encBuffer,size);
-}
-
-static void createBeaconList(){
+void createBeaconList(){
 	my_beacon.serviceGroupId=1;
 	my_beacon.has_name=true;
 	strcpy(my_beacon.name,"living room");
